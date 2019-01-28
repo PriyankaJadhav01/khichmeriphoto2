7d971f68b5abcc7ea5f5337cc81ef56bf13f55d4
Piyu <piya.011198@gmail.com>
2019-01-28 14:03:41 +0530

    Initial commit

---
+++ app/src/main/java/java/tata/piyu/khichmeriphoto2/Welcome.java
@@ -0,0 +1,41 @@
+package java.tata.piyu.khichmeriphoto2;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    Thread timer;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.release();
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        mediaPlayer=MediaPlayer.create(Welcome.this,R.raw.sound);
+        mediaPlayer.start();
+        timer=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(100000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent on= new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(on);
+                }
+            }
+        };
+        timer.start();;
+    }
+}
